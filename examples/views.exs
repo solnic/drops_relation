c07d21499423ac07cd042e19299a07ef7925a0c5
Peter Solnica <peter@solnica.online>
2025-07-17 06:02:37 +0000

    Update example

--- examples/views.exs
+++ examples/views.exs
@@ -1,10 +1,12 @@
 defmodule Users do
-  use Drops.Relation, repo: Drops.Relation.Repos.Postgres, name: "users"
+  use Drops.Relation, repo: Drops.Relation.Repos.Postgres
+
+  schema("users", infer: true)
 
   view(:active) do
     schema([:id, :name, :active])
 
-    relation do
+    derive do
       restrict(active: true)
     end
   end
@@ -14,3 +16,31 @@ Users.insert(%{name: "John", active: false})
 Users.insert(%{name: "Jane", active: true})
 Users.insert(%{name: "Joe", active: false})
 Users.insert(%{name: "Jade", active: true})
+
+IO.inspect(Users.last())
+# %Users.Schemas.User{
+#   __meta__: #Ecto.Schema.Metadata<:loaded, "users">,
+#   id: 4,
+#   name: "Jade",
+#   email: nil,
+#   age: nil,
+#   active: true,
+#   inserted_at: ~N[2025-07-16 21:56:47],
+#   updated_at: ~N[2025-07-16 21:56:47]
+# }
+
+IO.inspect(Users.active() |> Enum.to_list())
+# [
+#   %Users.Schemas.Active{
+#     __meta__: #Ecto.Schema.Metadata<:loaded, "users">,
+#     id: 2,
+#     name: "Jane",
+#     active: true
+#   },
+#   %Users.Schemas.Active{
+#     __meta__: #Ecto.Schema.Metadata<:loaded, "users">,
+#     id: 4,
+#     name: "Jade",
+#     active: true
+#   }
+# ]
