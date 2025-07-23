c78b2f6e44d3642163bd20302de291cff42fdd90
Peter Solnica <peter@solnica.online>
2025-07-23 07:11:30 +0000

    Remove empty module

--- lib/drops/relation/operations.ex
+++
@@ -1,8 +0,0 @@
-defmodule Drops.Relation.Operations do
-  @moduledoc """
-  Operations for composing and combining relation queries.
-  
-  This module provides operation structs that can be used to combine
-  multiple relation queries using logical operators like OR.
-  """
-end
