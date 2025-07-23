a0fcfb2ca7c57f2efa39ead43f2d78f1aedeef5b
Peter Solnica <peter@solnica.online>
2025-07-23 08:13:41 +0000

    Remove old test ecto schemas

--- test/support/setup.ex
+++ test/support/setup.ex
@@ -1,6 +1,3 @@
 Code.require_file("repos.ex", __DIR__)
 
-Code.require_file("ecto/test_schemas.ex", __DIR__)
-Code.require_file("ecto/user_group_schemas.ex", __DIR__)
-
 Application.ensure_all_started(:drops_relation)
